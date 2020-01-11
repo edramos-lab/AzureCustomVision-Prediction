64bea21fd161f23e51894c02227dd9d4a217e102
Bernd Verst <bverst@gmail.com>
2020-01-11 00:25:12 -0800

    Use Prediction API

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-import os, io, base64
+import os, io, base64, operator, random
 from flask import Flask, render_template, request, jsonify
+from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
+
+PREDICTION_KEY = os.environ['Prediction_Key']
+ENDPOINT = os.environ['Prediction_Endpoint']
+PROJECT_ID = os.environ['Project_Id']
+PUBLISH_ITERATION_NAME = os.environ['Iteration_Name']
+LABELS = ['rock', 'paper', 'scissors', 'lizard', 'spock']
 
 # Initialize the web application
 
@@ -17,9 +24,17 @@ def check_results():
     # Get the JSON passed to the request and extract the image
     body = request.get_json()
     image_bytes = base64.b64decode(body['image_base64'].split(',')[1])
-    image = io.BytesIO(image_bytes)
+    # image = io.BytesIO(image_bytes)
 
+    predictor = CustomVisionPredictionClient(PREDICTION_KEY, endpoint=ENDPOINT)
 
-    # return jsonify({'predicted': str(LABELS[highest_probability_index]),
-    #                 'probability': str(probability),
-    #                 'opponent': LABELS[np.random.randint(5)]})
+    results = predictor.classify_image(
+        PROJECT_ID, PUBLISH_ITERATION_NAME, image_bytes)
+    
+    predictions = {prediction.tag_name: prediction.probability for prediction in results.predictions}
+
+    predicted = max(predictions.keys(), key=(lambda k: predictions[k]))
+
+    return jsonify({'predicted': predicted,
+                    'probability': predictions[predicted],
+                    'opponent': LABELS[random.randint(0,4)]})
